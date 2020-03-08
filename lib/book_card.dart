472579bb01af0a17c28be86943e4d11db9f59ad5
jswolf19 <jswolf19@hotmail.com>
2020-03-08 18:29:38 +0900

    BookCard追加

---
+++ lib/book_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'book.dart';
+
+class BookCard extends StatefulWidget {
+  @override
+  _BookCardState createState() => _BookCardState();
+}
+
+class _BookCardState extends State<BookCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
