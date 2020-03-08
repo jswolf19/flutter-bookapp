cec9da4cc9f84088864af99895e8bab1fde8a853
jswolf19 <jswolf19@hotmail.com>
2020-03-08 17:21:13 +0900

    google books api使用

--- lib/book_page.dart
+++ lib/book_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class BookPage extends StatefulWidget {
   @override
@@ -6,12 +7,29 @@ class BookPage extends StatefulWidget {
 }
 
 class _BookPageState extends State<BookPage> {
+  String text = 'loading';
+
+  @override
+  void initState() {
+    super.initState();
+
+    requestSearchBooks();
+  }
+
+  void requestSearchBooks() async {
+    final response = await http.get('https://www.googleapis.com/books/v1/volumes/?q=factfulness');
+    setState(() {
+      text = response.body;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:  AppBar(
         title: Text('Book App'),
       ),
+      body: Text(text),
     );
   }
 }
