51e33a9fd1ae03747e4d1156bf521e8d4694b427
jswolf19 <jswolf19@hotmail.com>
2020-03-08 15:40:34 +0900

    画像の表示

--- lib/main.dart
+++ lib/main.dart
@@ -73,6 +73,12 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
+            Image.asset(
+              'assets/images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
             buttonSection,
             textSection,
