9389e7e21dd6bde7c9e116c057353b5a36542a3f
jswolf19 <jswolf19@hotmail.com>
2020-03-08 15:12:49 +0900

    3-2.8 Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -25,10 +25,12 @@ class MyApp extends StatelessWidget {
               height: 100,
               color: Colors.red,
             ),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.yellow,
+            Expanded(
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+              ),
             )
           ],
         ),
