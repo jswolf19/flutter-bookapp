c2118e682162cce9601f3b2e1f7c811a05a84184
jswolf19 <jswolf19@hotmail.com>
2020-03-14 22:03:53 +0900

    カリールのポーリング

--- lib/calil_connection.dart
+++ lib/calil_connection.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'book.dart';
 
@@ -5,16 +6,34 @@ class CalilConnection {
   final String systemId;
   final String _appKey;
 
-  CalilConnection({String appKey, this.systemId}) : _appKey = appKey;
+  final Duration _pollInterval;
+
+  CalilConnection({String appKey, this.systemId})
+      : _appKey = appKey,
+        _pollInterval = Duration(seconds: 2);
 
   void check(Book book) async {
-    _firstRequest(book.isbn);
+    Map result = await _firstRequest(book.isbn);
+    while(result["continue"] == 1) {
+      print("retry");
+      await Future.delayed(_pollInterval);
+      result = await _continueRequest(result["session"]);
+    }
+    print("finish");
   }
 
-  void _firstRequest(String isbn) async {
-    final url = 'https://api.calil.jp/check?appkey=$_appKey'
-                '&isbn=$isbn&systemid=$systemId&format=json&callback=no';
-    final response = await http.get(url);
+  Future<Map> _firstRequest(String isbn) async {
+    return _getResponse('appkey=$_appKey&isbn=$isbn&systemid=$systemId');
+  }
+
+  Future<Map> _continueRequest(String session) async {
+    return _getResponse('session=$session');
+  }
+
+  Future<Map> _getResponse(String query) async {
+    http.Response response = await http.get('https://api.calil.jp/check?$query'
+                                            '&format=json&callback=no');
     print(response.body);
+    return json.decode(response.body);
   }
 }
